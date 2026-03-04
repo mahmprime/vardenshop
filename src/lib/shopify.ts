5f7403eea9b0764f980a203a0449bd8db33761f5
Maksim <mahmprime@gmail.com>
2026-03-04 22:40:49 +0100

    benjo

--- src/lib/shopify.ts
+++ src/lib/shopify.ts
@@ -2,6 +2,6 @@
 import Client from 'shopify-buy'; // Promijenjeno sa '@shopify/buy' u 'shopify-buy'
 
 export const shopifyClient = Client.buildClient({
-  domain: 'varden-8392.myshopify.com',
+  domain: 'varden-1.myshopify.com',
   storefrontAccessToken: 'd8da9b08c9a73daa4688cd45796981a9'
 });
